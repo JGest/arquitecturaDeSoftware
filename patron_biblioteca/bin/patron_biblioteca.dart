ef11436eebb10533b242ff76d21bedceaa7619e5
JGest <201905558@est.umss.edu>
2025-05-09 22:27:17 -0400

    Subiendo el proyecto raiz

---
+++ patron_biblioteca/bin/patron_biblioteca.dart
@@ -0,0 +1,5 @@
+import 'package:patron_biblioteca/patron_biblioteca.dart' as patron_biblioteca;
+
+void main(List<String> arguments) {
+  print('Hello world: ${patron_biblioteca.calculate()}!');
+}
---
+++ patron_biblioteca/test/patron_biblioteca_test.dart
@@ -0,0 +1,8 @@
+import 'package:patron_biblioteca/patron_biblioteca.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
